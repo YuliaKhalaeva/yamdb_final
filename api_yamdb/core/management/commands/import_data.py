ce165941a926934f8faec4a9c4a34ddd27ab7ca5
Юля Халаева <juliakhalayeva2015@yandex.ru>
2023-03-03 14:18:15 +0300

    pep8

--- api_yamdb/core/management/commands/import_data.py
+++ api_yamdb/core/management/commands/import_data.py
@@ -1,8 +1,13 @@
-import os
 import csv
-from django.core.management.base import BaseCommand
+
+import os
+
 from django.conf import settings
+
+from django.core.management.base import BaseCommand
+
 from reviews.models import Category, Genre, Title, Review, Comment
+
 from users.models import User
 
 
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,7 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.core.validators import RegexValidator
+
 from django.conf import settings
+
+from django.contrib.auth.models import AbstractUser
+
+from django.core.validators import RegexValidator
+
 
 
 class User(AbstractUser):
